fc4d84e8a339c3e1fb23f34f1330c7f700c84884
akshatshandilya6869 <akshatshandilya203@gmail.com>
2025-03-05 23:23:49 +0530

    Implementing CRUD operation for Employee and connecting it to mysql database

---
+++ Payroll_UC2/src/test/java/com/example/EmployeePay/EmployeePayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EmployeePay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
